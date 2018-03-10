629b19f9090de31d8ad7557c21cf9b9bf9f4dec2
letry <letrybaz@yandex.ru>
2018-03-10 17:25:52 +0300

    addSorting

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const fs = require('fs');
-const util = require('util');
 const asker = require('./modules/ask');
 const flatTree = require('./modules/flatTree');
+
 const addToDictionary = require('./modules/addToDictionary');
+const translateDictionary = require('./modules/translateDictionary');
 const removeFromDictionary = require('./modules/removeFromDictionary');
 const selectDictionary = require('./modules/selectDictionary');
 const createDictionary = require('./modules/createDictionary');
+const outputDictionary = require('./modules/outputDictionary');
+const sortDictionary = require('./modules/sortDictionary');
+
 global.dictionary = `${__dirname}/dictionaries/default.json`;
 
-const upDown = [['По возрастанию'], ['По убыванию']];
+const fileOrInput = [['Ввести вручную'], ['Файл со словами']];
+const ascending = [['По возрастанию'], ['По убыванию']];
+
 const mainActions = [
   ['Выбрать рабочий словарь'],
   ['Создать словарь'],
-  ['Добавить в словарь', [
-    ['Ввести вручную'],
-    ['Файл со словами']
-  ]],
-  ['Удалить из словаря', [
-    ['Ввести вручную'],
-    ['Файл со словами']
-  ]],
+  ['Добавить в словарь', fileOrInput],
+  ['Удалить из словаря', fileOrInput],
   ['Упорядочить словарь', [
-    ['По алфавиту', upDown],
-    ['По количеству повторений', upDown]
+    ['По ключу', ascending],
+    ['По значению', ascending]
   ]],
   ['Перевести словарь'],
   ['Вывод результата'],
+  ['Трансформация словаря'],//Фильтры
   ['Выход']
 ];
 
@@ -33,7 +33,10 @@ const resolvers = [
   selectDictionary,
   createDictionary,
   addToDictionary,
-  removeFromDictionary,,,,
+  removeFromDictionary,
+  sortDictionary,
+  translateDictionary,
+  outputDictionary,,
   [() => process.exit()]
 ];
 
@@ -42,7 +45,12 @@ void async function main() {
   const answer = await asker(mainActions);
   const funcSequence = flatTree(resolvers, answer, 0, 1);
 
-  await funcSequence.reduceRight((result, func) => 
-    result = result.then(func), Promise.resolve());
+  try {
+    await funcSequence.reduceRight((result, func) => 
+      result = result.then(func), Promise.resolve());
+  } catch (error) {
+    console.error(error);
+  }
+
   main();
 }();
---
+++ modules/sortDictionary.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const ascending = [[() => 1], [() => -1]];
+
+module.exports = [
+    ({byValue, ascending}) => {
+        const dictionary = JSON.parse(fs.readFileSync(global.dictionary));
+        const entries = Object.entries(dictionary);
+        const mapper = val => +val[+byValue]//If is number then compare by char code
+            ? +val[+byValue] : val[+byValue].toString().charCodeAt();
+        
+        entries.sort((prev, next) => {
+            const [prevVal, nextVal] = [prev, next].map(mapper);
+            return (prevVal - nextVal) * ascending;
+        });
+
+        const result = entries.reduce((result, [key, value]) => Object.assign(result, {[key]: value}), {});
+        fs.writeFileSync(global.dictionary, JSON.stringify(result, null, '\t'));
+    },
+    [
+        [ascending => ({ byValue: false, ascending}), ascending],
+        [ascending => ({ byValue: true, ascending}), ascending],
+    ]
+];
